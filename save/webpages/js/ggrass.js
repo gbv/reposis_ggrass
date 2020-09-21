be1ac9a99eff01ddfc23c9b4d9595a5453d2fea7
Kathleen Neumann <neumann@gbv.de>
2020-09-21 11:29:40 +0200

    REP-498 fixed link and added spam protection js

--- save/webpages/js/ggrass.js
+++ save/webpages/js/ggrass.js
@@ -13,4 +13,12 @@ $(document).ready(function(){
    });
 
    $('.ggrass_tooltip').tooltip();
+
+   // spam protection for mails
+   $('span.madress').each(function(i) {
+       var text = $(this).text();
+       var address = text.replace(" [at] ", "@");
+       $(this).after('<a href="mailto:'+address+'">'+ address +'</a>')
+       $(this).remove();
+   });
 });
